ef8c616ecf53c5551cd9bf9138d9480c48d39be8
katyashnaider <tnt_kote_97.05@mail.ru>
2022-10-05 22:35:47 +0300

    Initial commit

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField] private SpawnPoint[] _spawnPoints;
+    [SerializeField] private Enemy _enemy;
+
+    private const float StepSecondSpawn = 2f;
+    private Coroutine _coroutine;
+
+    private void Start()
+    {
+        _coroutine = StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        var waitForSeconds = new WaitForSeconds(StepSecondSpawn);
+
+        foreach(var spawnPoint in _spawnPoints)
+        {
+            Instantiate(_enemy, spawnPoint.transform.position, Quaternion.identity);
+
+            yield return waitForSeconds;
+        }
+
+        StopCoroutine(_coroutine);
+    }
+}
